358219fd81d49bce6ddef45cb4816b271ba9be35
unknown <ahmriaz.official@gmail.com>
2021-06-20 20:33:12 +0600

    imgStoreOnServer

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './Inventory.css'
-import fakeData from "../../fakeData"
 
 const Inventory = () => {
     const handleAddProduct = () => {
+        const product = {};
         fetch('http://localhost:5000/addProduct', {
             method: 'POST',
             headers: { "content-type": "application/json" },
-            body: JSON.stringify(fakeData)
+            body: JSON.stringify(product)
         })
 
     }
     return (
         <div>
-            <button onClick={handleAddProduct}>Add Product</button>
+            <form onSubmit=''>
+                <p><span>Name: </span><input type="text"></input></p>
+                <p><span>Price: </span><input type="text"></input></p>
+                <p><span>Quantity: </span><input type="text"></input></p>
+                <p><span>Product Image: </span><input type="file"></input></p>
+                <button onClick={handleAddProduct}>Add Product</button>
+
+            </form>
+
         </div>
     );
 };
